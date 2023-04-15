4e6bd052bff5d5734b9da49bcaae0112e09f97ef
Huseyin Bilen <huseyinxbilen@gmail.com>
2023-04-15 22:51:51 +0300

    Chat Page Added

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
+import Login from './Login/Login';
+import Chat from './Chat/Chat';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}>
+        </Route>
+        <Route path='/chat' element={<Chat />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
