115df5e8f2801435fba4c1fcbb1046a243f86544
Huseyin Bilen <huseyinxbilen@gmail.com>
2023-04-18 23:04:36 +0300

    Login Method Completed

--- client/src/Login/Login.js
+++ client/src/Login/Login.js
@@ -1,45 +1,74 @@
 import "./Login.css";
 
 function Login() {
+  const loginMethod = (e) => {
+    e.preventDefault();
+    let email = document.getElementById("login-email").value;
+    let password = document.getElementById("login-pass").value;
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email: email, password: password }),
+    };
+    fetch("http://localhost:3001/login", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        // Token storage
+        document.cookie = `token=${data.data.token}; expires=${new Date(new Date().setFullYear(new Date().getFullYear() + 1))}; path=/`;
+      });
+  };
+
   return (
     <div className="App">
       <div className="App-body">
         <div className="login-section">
-          <form>
+          <form onSubmit={loginMethod}>
             <input
+              id="login-email"
               type="text"
-              placeholder="E-mail or username"
+              placeholder="E-mail"
               className="input input-bordered input-accent w-full max-w-xs"
-            /><br/>
+            />
+            <br />
             <input
+              id="login-pass"
               type="password"
               placeholder="Password"
               className="input input-bordered input-accent w-full max-w-xs mt-5"
             />
             <div className="grid h-20 card rounded-box place-items-center">
-              <button className="btn btn-success">Login</button>
+              <button className="btn btn-success" type="submit">
+                Login
+              </button>
             </div>
           </form>
           <div className="divider">OR</div>
           <div className="register-section">
-            <form action="/chat">
+            <form onSubmit={registerMethod}>
               <input
+                id="register-email"
                 type="text"
                 placeholder="E-mail"
                 className="input input-bordered input-success w-full max-w-xs"
-              /><br/>
+              />
+              <br />
               <input
+                id="register-username"
                 type="text"
                 placeholder="Username"
                 className="input input-bordered input-success w-full max-w-xs mt-5"
               />
               <input
+                id="register-pass"
                 type="password"
                 placeholder="Password"
                 className="input input-bordered input-success w-full max-w-xs mt-5"
               />
               <div className="grid h-20 card rounded-box place-items-center">
-                <button className="btn btn-info" type="submit">Register</button>
+                <button className="btn btn-info" type="submit">
+                  Register
+                </button>
               </div>
             </form>
           </div>
