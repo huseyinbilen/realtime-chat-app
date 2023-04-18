109ef6d2ef7f864aa3d0381c13bafa0cdeeb064a
Huseyin Bilen <huseyinxbilen@gmail.com>
2023-04-18 23:09:01 +0300

    Register Method Completed

--- client/src/Login/Login.js
+++ client/src/Login/Login.js
@@ -15,10 +15,37 @@ function Login() {
       .then((response) => response.json())
       .then((data) => {
         // Token storage
-        document.cookie = `token=${data.data.token}; expires=${new Date(new Date().setFullYear(new Date().getFullYear() + 1))}; path=/`;
+        document.cookie = `token=${data.data.token}; expires=${new Date(
+          new Date().setFullYear(new Date().getFullYear() + 1)
+        )}; path=/`;
       });
   };
 
+  const registerMethod = (e) => {
+    e.preventDefault();
+    let email = document.getElementById("register-email").value;
+    let username = document.getElementById("register-username").value;
+    let password = document.getElementById("register-pass").value;
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: email,
+        username: username,
+        password: password,
+      }),
+    };
+    fetch("http://localhost:3001/register", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        alert(data.desc+"\nPlease Login!");
+        console.log(data);
+      });
+    document.getElementById("register-email").value = "";
+    document.getElementById("register-username").value = "";
+    document.getElementById("register-pass").value = "";
+  };
   return (
     <div className="App">
       <div className="App-body">
