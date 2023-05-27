a5a5e86093a516917cc462fbcd1ecd688f5c167a
Huseyin Bilen <huseyinxbilen@gmail.com>
2023-05-27 20:48:00 +0300

    Login Tests Added

--- server/tests/pageControllers.test.js
+++ server/tests/pageControllers.test.js
@@ -14,6 +14,18 @@ afterEach(async () => {
   await mongoose.connection.close();
 });
 
+afterAll((done) => {
+  app.close(done);
+});
+
+
+describe("GET /", () => {
+  it("should get Welcome Page", async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toBe(200);
+  });
+});
+
 describe("POST /register", () => {
   it("should create a user", async () => {
     const res = await request(app).post("/register").send({
@@ -23,5 +35,16 @@ describe("POST /register", () => {
     });
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBe("success");
+  });
+});
+
+describe("POST /login", () => {
+  it("should login a user", async () => {
+    const res = await request(app).post("/login").send({
+      email: "user@test.com",
+      password: "testpass"
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.success).toBe(true);
   });
 });
